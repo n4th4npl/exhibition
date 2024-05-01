2297c28bb400b1a2acc2b35dcaee95e0cf3afbdb
n4th4npl <nathanloveux@gmail.com>
2024-05-01 15:43:37 +0100

    new assets

---
+++ scripts.js
@@ -0,0 +1,41 @@
+// document.querySelector('#exhibition-1st').scrollIntoView({
+//     behavior: 'smooth',
+//     block: 'start',
+//     inline: 'nearest',
+//     offsetTop: -200 // Adjust the value as needed
+// });
+
+// document.querySelector('#exhibition-2nd').scrollIntoView({
+//     behavior: 'smooth',
+//     block: 'start',
+//     inline: 'nearest',
+//     offsetTop: -100 // Adjust the value as needed
+// });
+
+// document.querySelector('#exhibition-3rd').scrollIntoView({
+//     behavior: 'smooth',
+//     block: 'start',
+//     inline: 'nearest',
+//     offsetTop: -100 // Adjust the value as needed
+// });
+
+// document.querySelector('#exhibition-4th').scrollIntoView({
+//     behavior: 'smooth',
+//     block: 'start',
+//     inline: 'nearest',
+//     offsetTop: -150 // Adjust the value as needed
+// });
+
+// document.querySelector('#exhibition-5th').scrollIntoView({
+//     behavior: 'smooth',
+//     block: 'start',
+//     inline: 'nearest',
+//     offsetTop: -10 // Adjust the value as needed
+// });
+
+// document.querySelector('#exhibition-6th').scrollIntoView({
+//     behavior: 'smooth',
+//     block: 'start',
+//     inline: 'nearest',
+//     offsetTop: -150 // Adjust the value as needed
+// });
